8f69e89fab8d9754f82188fe0cd6810d685c35c9
Ahram Lee <alee239@myseneca.ca>
2024-10-02 16:21:50 -0400

    Initial commit

---
+++ js/songs.js
@@ -0,0 +1,213 @@
+/**
+ * songs.js
+ *
+ * The app's songs
+ */
+
+window.songs = [
+  // BTS Songs
+  {
+    songId: "SID-2001",
+    artistId: "AID-1001",
+    title: "Dynamite",
+    released: 2020,
+    duration: 199,
+    mediaUrl: "https://www.youtube.com/watch?v=gdZLi9oWNZg",
+    explicitLyrics: false,
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/5/5f/BTS_-_Dynamite_%28official_cover%29.png"
+  },
+  {
+    songId: "SID-2002",
+    artistId: "AID-1001",
+    title: "Butter",
+    released: 2021,
+    duration: 164,
+    mediaUrl: "https://www.youtube.com/watch?v=WMweEpGlu_U",
+    explicitLyrics: false,
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/d/db/BTS_-_Butter.png"
+  },
+  {
+    songId: "SID-2003",
+    artistId: "AID-1001",
+    title: "Permission to Dance",
+    released: 2021,
+    duration: 224,
+    mediaUrl: "https://www.youtube.com/watch?v=CuklIb9d3fI",
+    explicitLyrics: false,
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/0/04/BTS_-_Permission_to_Dance.png"
+  },
+  {
+    songId: "SID-2004",
+    artistId: "AID-1001",
+    title: "Boy With Luv",
+    released: 2019,
+    duration: 230,
+    mediaUrl: "https://www.youtube.com/watch?v=XsX3ATc3FbA",
+    explicitLyrics: false,
+    imageUrl: "https://i1.sndcdn.com/artworks-cJhzdvRT8aUQ3fY1-yglEKg-t500x500.jpg"
+  },
+  {
+    songId: "SID-2005",
+    artistId: "AID-1001",
+    title: "Fake Love",
+    released: 2018,
+    duration: 246,
+    mediaUrl: "https://www.youtube.com/watch?v=7C2z4GqqS5E",
+    explicitLyrics: false,
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/8/84/Fake_Love_%28Rocking_Vibe_Mix%29_Cover_Art.jpg"
+  },
+  {
+    songId: "SID-2006",
+    artistId: "AID-1001",
+    title: "Spring Day",
+    released: 2017,
+    duration: 266,
+    mediaUrl: "https://www.youtube.com/watch?v=xEeFrLSkMm8",
+    explicitLyrics: false,
+    imageUrl: "https://ibighit.com/bts/images/bts/discography/you_never_walk_alone/album-cover.jpg"
+  },
+  {
+    songId: "SID-2007",
+    artistId: "AID-1001",
+    title: "Fire",
+    released: 2016,
+    duration: 206,
+    mediaUrl: "https://www.youtube.com/watch?v=ALj5MKjy2BU",
+    explicitLyrics: false,
+    imageUrl: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/95a3ad4c-f8e5-42b3-8ac2-7d209e6db374/dawvjv2-dce592e3-3a03-4358-b4d1-ce36311573b7.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzk1YTNhZDRjLWY4ZTUtNDJiMy04YWMyLTdkMjA5ZTZkYjM3NFwvZGF3dmp2Mi1kY2U1OTJlMy0zYTAzLTQzNTgtYjRkMS1jZTM2MzExNTczYjcuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.tpr0bhI6kH7NBAqvoWlobiwm3GbPw9bEVFwuuVGU-M4"
+  },
+
+  // BLACKPINK Songs
+  {
+    songId: "SID-2008",
+    artistId: "AID-1002",
+    title: "DDU-DU DDU-DU",
+    released: 2018,
+    duration: 209,
+    mediaUrl: "https://www.youtube.com/watch?v=IHNzOHi8sJs",
+    explicitLyrics: false,
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/8/8b/Black_Pink_-_Ddu-Du_Ddu-Du_%28Japanese_version%29_artwork.png"
+  },
+  {
+    songId: "SID-2009",
+    artistId: "AID-1002",
+    title: "How You Like That",
+    released: 2020,
+    duration: 181,
+    mediaUrl: "https://www.youtube.com/watch?v=ioNng23DkIM",
+    explicitLyrics: false,
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Blackpink_-_How_You_Like_That.png/220px-Blackpink_-_How_You_Like_That.png"
+  },
+  {
+    songId: "SID-2010",
+    artistId: "AID-1002",
+    title: "Kill This Love",
+    released: 2019,
+    duration: 212,
+    mediaUrl: "https://www.youtube.com/watch?v=2S24-y0Ij3Y",
+    explicitLyrics: false,
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/b/bc/Blackpink_-_Kill_This_Love.png"
+  },
+  {
+    songId: "SID-2011",
+    artistId: "AID-1002",
+    title: "BOOMBAYAH",
+    released: 2016,
+    duration: 248,
+    mediaUrl: "https://www.youtube.com/watch?v=bwmSjveL3Lc",
+    explicitLyrics: false,
+    imageUrl: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bcc96d39-785d-4796-bb89-fede2ff7fe12/deobb5f-ada3d253-b1f2-4dba-b907-ca4d518ea94f.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2JjYzk2ZDM5LTc4NWQtNDc5Ni1iYjg5LWZlZGUyZmY3ZmUxMlwvZGVvYmI1Zi1hZGEzZDI1My1iMWYyLTRkYmEtYjkwNy1jYTRkNTE4ZWE5NGYuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.iT8R9TOEOq2-0_rCz_vWzATHgS2HSRRvXDyX6IJaCgQ"
+  },
+  {
+    songId: "SID-2012",
+    artistId: "AID-1002",
+    title: "As If It's Your Last",
+    released: 2017,
+    duration: 213,
+    mediaUrl: "https://www.youtube.com/watch?v=Amq-qlqbjYA",
+    explicitLyrics: false,
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/6/66/BLACK_PINK_-_As_If_It%27s_Your_Last.jpg"
+  },
+  {
+    songId: "SID-2013",
+    artistId: "AID-1002",
+    title: "Playing with Fire",
+    released: 2016,
+    duration: 197,
+    mediaUrl: "https://www.youtube.com/watch?v=9pdj4iJD08s",
+    explicitLyrics: false,
+    imageUrl: "https://i1.sndcdn.com/artworks-000541190028-di8peg-t500x500.jpg"
+  },
+  {
+    songId: "SID-2014",
+    artistId: "AID-1002",
+    title: "Whistle",
+    released: 2016,
+    duration: 217,
+    mediaUrl: "https://www.youtube.com/watch?v=dISNgvVpWlo",
+    explicitLyrics: false,
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/f/f8/Square_One_-_Blackpink.jpg"
+  },
+
+  // IU Songs
+  {
+    songId: "SID-2015",
+    artistId: "AID-1003",
+    title: "Good Day",
+    released: 2010,
+    duration: 236,
+    mediaUrl: "https://www.youtube.com/watch?v=jeqdYqsrsA0",
+    explicitLyrics: false,
+    imageUrl: "https://i1.sndcdn.com/artworks-000090881151-jg71ro-t500x500.jpg"
+  },
+  {
+    songId: "SID-2016",
+    artistId: "AID-1003",
+    title: "Palette",
+    released: 2017,
+    duration: 213,
+    mediaUrl: "https://www.youtube.com/watch?v=d9IxdwEFk1c",
+    explicitLyrics: false,
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/b/b6/IU_Palette_final.jpg"
+  },
+  {
+    songId: "SID-2017",
+    artistId: "AID-1003",
+    title: "Blueming",
+    released: 2019,
+    duration: 210,
+    mediaUrl: "https://www.youtube.com/watch?v=D1PvIWdJ8xo",
+    explicitLyrics: false,
+    imageUrl: "https://i1.sndcdn.com/artworks-000642097669-ltmdh8-t500x500.jpg"
+  },
+  {
+    songId: "SID-2018",
+    artistId: "AID-1003",
+    title: "Eight",
+    released: 2020,
+    duration: 174,
+    mediaUrl: "https://www.youtube.com/watch?v=TgOu00Mf3kI",
+    explicitLyrics: false,
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/c/c2/IU_EIGHT_FT_SUGA.jpg"
+  },
+  {
+    songId: "SID-2019",
+    artistId: "AID-1003",
+    title: "Celebrity",
+    released: 2021,
+    duration: 203,
+    mediaUrl: "https://www.youtube.com/watch?v=0-q1KafFCLU",
+    explicitLyrics: false,
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/1/1d/Celebrity_IU_cover.jpg"
+  },
+  {
+    songId: "SID-2020",
+    artistId: "AID-1003",
+    title: "BBIBBI",
+    released: 2018,
+    duration: 187,
+    mediaUrl: "https://www.youtube.com/watch?v=nM0xDI5R50E",
+    explicitLyrics: false,
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/6/63/IU_%E2%80%93_Bbibbi.png"
+  }
+]
